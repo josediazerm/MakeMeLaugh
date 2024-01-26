3d7ef7e5bb868934f65ab8c1e532b2be8da3e61f
unknown <josemariadiazdeharo@gmail.com>
2024-01-26 19:27:35 +0100

    Created basic card

---
+++ Make Me Laugh/Resource/Autoload/constants.gd
@@ -0,0 +1,10 @@
+# Path constants
+
+const LOCAL_SAVE_DATA_PATH: String = "res://save_data.tres"
+const USER_SAVE_DATA_PATH: String = "user://save_data.tres"
+
+# --------------------------------------------------------------------------- #
+
+# Images constants
+
+const DEFAULT_IMAGE : String = "res://icon.svg"
