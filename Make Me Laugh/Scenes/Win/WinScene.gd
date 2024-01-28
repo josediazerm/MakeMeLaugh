22812e2e9fa9e75b2acf152e0e5c97de5a86777a
unknown <josemariadiazdeharo@gmail.com>
2024-01-28 14:59:48 +0100

    free

--- Make Me Laugh/Scenes/Win/WinScene.gd
+++ Make Me Laugh/Scenes/Win/WinScene.gd
@@ -2,15 +2,23 @@ extends Node2D
 
 @export var data_manager : Node2D
 
-@export var label : Label
+@export var music_manager : Node2D
 
 var cards_played 
 
 
 func _ready():
 	cards_played = data_manager.get_cards_played()
+	print(cards_played)
 	find_most_played_card()
-	
+
+	music_manager.play(Constants.MENU_SOLO_INICIO_MUSIC )
+
+
+func _on_music_finished():
+
+	music_manager.play(Constants.MENU_SIN_INICIO_MUSIC )
+
 
 func find_most_played_card():
 	var deck_freq = {}
@@ -23,19 +31,22 @@ func find_most_played_card():
 
 	var most_played_card_value = 0
 	var most_played_card_key = ""
-
+	
+	print(deck_freq)
+	print(most_played_card_value)
 	for key in deck_freq.keys():
 		if deck_freq[key] > most_played_card_value:
 			most_played_card_key = key
 			most_played_card_value = deck_freq[key]
 			
-	label.text = most_played_card_key
 	
 	
 	var str = most_played_card_key.split("_")
 
 	var rec_str = "%s/%s/%s_%s%s"
 	rec_str = rec_str % [Constants.DEFAULT_CHISTES, str[1], str[2], str[3], Constants.SCENE_SUFFIX]
+	
+	print(rec_str)
 	
 	var card_load = load(rec_str)
 	var card_instance =  card_load.instantiate()
